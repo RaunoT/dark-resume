991d8b71c66ed00dd0373fa88530ac56aeb7404d
RaunoT <RaunoT@users.noreply.github.com>
2019-03-07 22:30:05 +0200

    More animations, added dev icons

--- js/functions.js
+++ js/functions.js
@@ -1,174 +1,183 @@
+// Use strict
 (function ($) {
-	"use strict"; // Start of use strict
+	"use strict";
+	$(document).ready(function () {
 
-	// Smooth scrolling using jQuery easing
-	$('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
-		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-			var target = $(this.hash);
-			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-			if (target.length) {
-				$('html, body').animate({
-					scrollTop: (target.offset().top)
-				}, 750, "easeInOutExpo");
-				return false;
+		$(document).on('click', function () {
+			// Close mobile-nav if user clicks on body
+			if ($('.navbar-collapse').hasClass('show')) {
+				$('.navbar-collapse').collapse('hide');
+				setTimeout(function () {
+					$('#side-nav').css("background", "transparent");
+				}, 200);
 			}
-		}
-	});
+		});
 
-	// Closes responsive menu when a scroll trigger link is clicked
-	$('.js-scroll-trigger').click(function () {
-		$('.navbar-collapse').collapse('hide');
-	});
+		$(document).on('scroll', function () {
+			// Close mobile-nav if user scrolls
+			if ($('.navbar-collapse').hasClass('show')) {
+				$('.navbar-collapse').collapse('hide');
+				setTimeout(function () {
+					$('#side-nav').css("background", "transparent");
+				}, 200);
+			}
+		});
 
-})(jQuery); // End of use strict
+		// Smooth scrolling using jQuery easing
+		$('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+			if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+				var target = $(this.hash);
+				target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+				if (target.length) {
+					$('html, body').animate({
+						scrollTop: (target.offset().top - 40)
+					}, 750, "easeInOutExpo");
+					return false;
+				}
+			}
+		});
 
-$(document).ready(function () {
+		// Make open mobile-nav background opaque
+		$(".navbar").children().click(function () {
+			if ($('.navbar-toggler').hasClass('collapsed')) {
+				$('#side-nav').css("background", "#110f01");
+			} else {
+				setTimeout(function () {
+					$('#side-nav').css("background", "transparent");
+				}, 200);
+			}
+		});
 
-	// Animations on waypoint
-	$(".social-icons").waypoint(function () {
-		if (!$(this.element).hasClass("animation-complete")) {
-			$(".social-icons > a").addClass("animated zoomIn");
-			$(".social-icons").css("visibility", "visible");
+		// Close mobile-nav when a scroll trigger is clicked
+		$('.js-scroll-trigger').click(function () {
+			$('.navbar-collapse').collapse('hide');
 			setTimeout(function () {
-				$(".social-icons > a").removeClass("animated zoomIn");
-				$(".social-icons > a").addClass("hvr-float");
-				$(".social-icons").addClass("animation-complete");
-			}, 1000);
+				$('#side-nav').css("background", "transparent");
+			}, 200);
+		});
+
+		// fancyBox
+		$("[data-fancybox]").fancybox({
+			toolbar: false
+		});
+
+		// Animations on waypoint
+		$(".social-icons").waypoint(function () {
+			if (!$(this.element).hasClass("animation-complete")) {
+				$(".social-icons > a").addClass("animated zoomIn");
+				$(".social-icons").css("visibility", "visible");
+				// Wait for animation to complete
+				setTimeout(function () {
+					$(".social-icons > a").removeClass("animated zoomIn");
+					$(".social-icons").addClass("animation-complete");
+					// Don't add hover effect on mobile
+					if ($(window).width() >= 992) {
+						$(".social-icons > a").addClass("hvr-shrink");
+					}
+				}, 1000);
+			}
+		}, {
+			offset: "80%"
+		});
+
+		// Don't add hover effect on mobile
+		if ($(window).width() >= 992) {
+			$(".dev-icons > li > i").addClass("hvr-float");
 		}
-	}, {
-		offset: "80%"
-	});
 
-	$("#waypoint-1").waypoint(function () {
-		$(".dev-icons").addClass("animated bounceInRight");
-		$(".dev-icons").css("visibility", "visible");
-	}, {
-		offset: "100%"
-	});
-
-	$("#waypoint-2").waypoint(function () {
-		$("#languages").addClass("animated bounceInRight");
-		$("#languages").css("visibility", "visible");
-	}, {
-		offset: "100%"
-	});
-
-	$("#waypoint-3").waypoint(function () {
-		$("#workflow").addClass("animated bounceInRight");
-		$("#workflow").css("visibility", "visible");
-	}, {
-		offset: "100%"
-	});
-
-	$("#waypoint-4").waypoint(function () {
-		$("#education-content-1").addClass("animated fadeInUp");
-		$("#education-content-1").css("visibility", "visible");
-	}, {
-		offset: "100%"
-	});
-
-	$("#waypoint-5").waypoint(function () {
-		$("#education-content-2").addClass("animated fadeInUp");
-		$("#education-content-2").css("visibility", "visible");
-	}, {
-		offset: "90%"
-	});
-
-	/* 	$("#waypoint-6").waypoint(function () {
-			$("#experience-1").addClass("animated fadeInUp");
-			$("#experience-1").css("visibility", "visible");
+		$("#waypoint-1").waypoint(function () {
+			$(".dev-icons").addClass("animated bounceInRight");
+			$(".dev-icons").css("visibility", "visible");
 		}, {
 			offset: "100%"
-		}); */
+		});
 
-	$("#waypoint-7").waypoint(function () {
-		$("#project-1").addClass("animated fadeInRight");
-		$("#project-1").css("visibility", "visible");
-	}, {
-		offset: "120%"
-	});
+		$("#waypoint-2").waypoint(function () {
+			$("#languages").addClass("animated bounceInRight");
+			$("#languages").css("visibility", "visible");
+		}, {
+			offset: "100%"
+		});
 
-	$("#waypoint-8").waypoint(function () {
-		$("#project-2").addClass("animated fadeInRight");
-		$("#project-2").css("visibility", "visible");
-	}, {
-		offset: "120%"
-	});
+		$("#waypoint-3").waypoint(function () {
+			$("#workflow").addClass("animated bounceInRight");
+			$("#workflow").css("visibility", "visible");
+		}, {
+			offset: "100%"
+		});
 
-	$("#waypoint-9").waypoint(function () {
-		$("#project-3").addClass("animated fadeInRight");
-		$("#project-3").css("visibility", "visible");
-	}, {
-		offset: "120%"
-	});
+		$("#waypoint-4").waypoint(function () {
+			$("#education-content-1").addClass("animated fadeInUp");
+			$("#education-content-1").css("visibility", "visible");
+		}, {
+			offset: "100%"
+		});
 
-	$(".img-project-cover").waypoint(function () {
-		if (!$(this.element).hasClass("animation-complete")) {
-			$(this.element).addClass('animated fadeInLeft');
-			$(this.element).css("visibility", "visible");
-			var currentImage = $(this.element);
-			setTimeout(function () {
-				$(currentImage).removeClass("animated fadeInLeft");
-				$(currentImage).addClass("hvr-backward");
-				$(currentImage).addClass("animation-complete");
-			}, 1000);
-		}
-	}, {
-		offset: "95%"
-	});
+		$("#waypoint-5").waypoint(function () {
+			$("#education-content-2").addClass("animated fadeInUp");
+			$("#education-content-2").css("visibility", "visible");
+		}, {
+			offset: "90%"
+		});
 
-	$("hr").waypoint(function () {
-		$(this.element).addClass('grow');
-	}, {
-		offset: "75%"
-	});
+		/* 	$("#waypoint-6").waypoint(function () {
+				$("#experience-1").addClass("animated fadeInUp");
+				$("#experience-1").css("visibility", "visible");
+			}, {
+				offset: "100%"
+			}); */
 
-	if ($(window).width() > 992) {
-		$("#project-1").addClass("delay");
-		$("#project-2").addClass("delay");
-		$("#project-3").addClass("delay");
-	}
+		$("#waypoint-7").waypoint(function () {
+			$("#project-1").addClass("animated fadeInRight");
+			$("#project-1").css("visibility", "visible");
+		}, {
+			offset: "120%"
+		});
 
-	// fancyBox
-	$("[data-fancybox]").fancybox({
-		toolbar: false
-	});
+		$("#waypoint-8").waypoint(function () {
+			$("#project-2").addClass("animated fadeInRight");
+			$("#project-2").css("visibility", "visible");
+		}, {
+			offset: "120%"
+		});
 
-	// Make open mobile-nav background opaque
-	$(".navbar").children().click(function () {
-		if ($('.navbar-toggler').hasClass('collapsed')) {
-			$('#side-nav').css("background", "#110f01");
-		} else {
-			setTimeout(function () {
-				$('#side-nav').css("background", "transparent");
-			}, 200);
-		}
-	});
+		$("#waypoint-9").waypoint(function () {
+			$("#project-3").addClass("animated fadeInRight");
+			$("#project-3").css("visibility", "visible");
+		}, {
+			offset: "120%"
+		});
 
-	// Close mobile-nav if user clicks on nav-link
-	$('.nav-link').click(function () {
-		setTimeout(function () {
-			$('#side-nav').css("background", "transparent");
-		}, 200);
-	});
+		$(".img-project-cover").waypoint(function () {
+			if (!$(this.element).hasClass("animation-complete")) {
+				$(this.element).addClass('animated fadeInLeft');
+				$(this.element).css("visibility", "visible");
+				var currentImage = $(this.element);
+				// Wait for animation to complete
+				setTimeout(function () {
+					$(currentImage).removeClass("animated fadeInLeft");
+					$(currentImage).addClass("animation-complete");
+					// Don't add hover effect on mobile
+					if ($(window).width() >= 992) {
+						$(currentImage).addClass("hvr-backward");
+					}
+				}, 1000);
+			}
+		}, {
+			offset: "95%"
+		});
 
-	$(document).on('click', function () {
-		// Close mobile-nav if user clicks on body
-		if ($('.navbar-collapse').hasClass('show')) {
-			$('.navbar-collapse').collapse('hide');
-			setTimeout(function () {
-				$('#side-nav').css("background", "transparent");
-			}, 200);
-		}
-	});
+		$("hr").waypoint(function () {
+			$(this.element).addClass('grow');
+		}, {
+			offset: "75%"
+		});
 
-	$(document).on('scroll', function () {
-		// Close mobile-nav if user scrolls
-		if ($('.navbar-collapse').hasClass('show')) {
-			$('.navbar-collapse').collapse('hide');
-			setTimeout(function () {
-				$('#side-nav').css("background", "transparent");
-			}, 200);
+		// Don't add hover effect on mobile
+		if ($(window).width() >= 992) {
+			$("#project-1").addClass("delay");
+			$("#project-2").addClass("delay");
+			$("#project-3").addClass("delay");
 		}
 	});
-});
+})(jQuery);
